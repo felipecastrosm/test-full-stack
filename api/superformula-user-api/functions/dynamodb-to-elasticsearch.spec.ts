6b02bfc0e974686124958b7fe5ee4905a4861b79
Felipe Castro <felipecastrosm@gmail.com>
2020-08-01 00:23:45 -0300

    Created unit tests for dynamodb-to-elasticsearch lambda

---
+++ api/superformula-user-api/functions/dynamodb-to-elasticsearch.spec.ts
@@ -0,0 +1,86 @@
+import { handler } from "./dynamodb-to-elasticsearch";
+
+import { Client } from '@elastic/elasticsearch';
+// import Mock from '@elastic/elasticsearch-mock';
+// const elasticsearchMock = new Mock();
+// const elasticsearchConnection = elasticsearchMock.getConnection();
+
+jest.mock('@elastic/elasticsearch');
+
+const baseEvent = {
+    Records: [
+        {
+            dynamodb: {
+                Keys: {
+                    id: {
+                        S: "sample_id"
+                    }
+                },
+                NewImage: {
+                    sample_field: {}
+                }
+            },
+            eventName: "INSERT" as 'INSERT' | 'MODIFY' | 'REMOVE'
+        }
+    ]
+};
+
+test("Adds new record to Elasticsearch", async () => {
+    //Arrange
+    baseEvent.Records[0].eventName = "INSERT";
+    Client.prototype.index = jest.fn().mockImplementation(() => {
+        return;
+    });
+
+    //Act
+    await handler(baseEvent, null);
+
+    //Assert
+    expect(Client.prototype.index).toHaveBeenCalledTimes(1);
+    expect(Client.prototype.index).toHaveBeenCalledWith({id: "sample_id", index: "user", body: { sample_field: {}}});
+});
+
+test("Updates record on Elasticsearch", async () => {
+    //Arrange
+    baseEvent.Records[0].eventName = "MODIFY";
+    Client.prototype.index = jest.fn().mockImplementation(() => {
+        return;
+    });
+
+    //Act
+    await handler(baseEvent, null);
+
+    //Assert
+    expect(Client.prototype.index).toHaveBeenCalledTimes(1);
+    expect(Client.prototype.index).toHaveBeenCalledWith({id: "sample_id", index: "user", body: { sample_field: {}}});
+});
+
+test("Removes record from Elasticsearch", async () => {
+    //Arrange
+    baseEvent.Records[0].eventName = "REMOVE";
+    Client.prototype.delete = jest.fn().mockImplementation(() => {
+        return;
+    });
+
+    //Act
+    await handler(baseEvent, null);
+
+    //Assert
+    expect(Client.prototype.delete).toHaveBeenCalledTimes(1);
+    expect(Client.prototype.delete).toHaveBeenCalledWith({id: "sample_id", index: "user"})
+});
+
+test("Processes multiple records", async () => {
+    //Arrange
+    baseEvent.Records[0].eventName = "INSERT";
+    baseEvent.Records.push(baseEvent.Records[0]);
+    Client.prototype.index = jest.fn().mockImplementation(() => {
+        return;
+    });
+
+    //Act
+    await handler(baseEvent, null);
+
+    //Assert
+    expect(Client.prototype.index).toHaveBeenCalledTimes(2);
+});
